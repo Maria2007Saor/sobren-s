326a2a5280a5d548f0298cd4d788483768e232ce
Maria2007Saor <maria.soares.barbosa14@escola.pr.gov.br>
2025-06-13 15:32:34 -0300

    Add files via upload

---
+++ nossa.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <div id="cabecalho">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transition bgc">
+            <a class="navbar-brand" href="#">
+                <img class="imagem" src="./Imagem_connect (2) - Copia.png" width="80" height="80" alt="">
+            </a>
+         
+        </div>
+         
+        </nav>
+         
+            </div>
+        </nav>
+    </div>
+
+    <div id="texto_imagens">
+        <div id="texto">
+
+            <div class="primeiro_texto">
+                <h1 style="color: #136167;">Nossa História</h1>
+
+                <p id= "p1">
+                    4 alunos do curso de desenvolvedor Web do Senac - Cornélio Procópio se uniram com o objetivo de
+                    projetar um site que tivesse impacto social. O grupo percebeu uma grande dificuldade na cidade: a
+                    falta de uma
+                    plataforma eficiente para conectar comerciantes locais a consumidores.
+                    Muitos pequenos negócios lutam para ganhar visibilidade, enquanto moradores têm dificuldade em
+                    encontrar serviços e
+                    produtos, e precisam recorrer a grandes redes ou viajar para cidades vizinhas. Inspirados por essa
+                    necessidade, os
+                    quatro amigos decidiram criar o "Conect+", um site voltado exclusivamente para a divulgação dos
+                    comerciantes e
+                    profissionais da região.
+                    Falar o que cada integrante fez durante o projeto.
+                    Falar do lançamento.
+                </p>
+            </div>
+
+            <div class="segundo_texto">
+                <h1 style="color: #136167;">Quem Somos</h1>
+
+                <p>
+                    Em 2025, um grupo de programadores de Cornélio Procópio, uma pequena cidade localizada no interior
+                    do Paraná, Brasil, decidiu unir suas forças e conhecimentos para criar algo que pudesse ajudar e
+                    melhorar pequenos e
+                    médios empresários ajudando o comércio local.
+                    Foi então que, após inúmeras reuniões e discussões, surgiu a ideia da CONNECT+, um site de buscas
+                    por serviços com
+                    um propósito claro: oferecer ajuda para as pessoas encontrar pequenos comércios na cidade e ajudar
+                    comerciantes a
+                    ampliar . A missão da empresa é simples, mas de grande impacto: proporcionar tratamentos eficazes,
+                    com preços
+                    justos, e que realmente façam a diferença na vida das pessoas.
+                </p>
+            </div>
+        </div>
+
+        <div id="images">
+            <div>
+                <img src="./assets/image@2x.png" alt="" class="img_3" style="padding-left: 175%; display:flex;">
+            </div>
+
+            <di>
+                <img src="./assets/imagemmmmm.png" alt="" class="img_4" style="padding-left: 175%; display:flex;">
+            </di>
+        </div>
+
+    </div>
+    <hr style="height: 1px; border-width: 0; background-color: #3a5f37" />
+
+<footer>
+    <ul style="display: flex; padding-left: 42%;">
+      <li><a href="" class="infos">Contato</a></li>
+      <li><a href="" class="infos">Quem Somos</a></li>
+      <li><a href="" class="infos">Política de Devolução</a></li>
+    </ul>
+    <div style="display: flex; padding-left: 10%">
+      <ul style="display: block">
+        <li><b>Departamentos</b></li>
+        <li>Início</li>
+        <li>Lançamentos</li>
+        <li>Recomendados</li>
+        <li>Leilões</li>
+        <li>Ofertas</li>
+        <li>Anuncia</li>
+      </ul>
+
+      <ul style="display: block; padding-left: 10%">
+        <li><b>Entre em contato</b></li>
+        <li>Fone: ‪+55 43 99999-9999‬</li>
+        <li>E-mail: OldBazzar51@gmail.com</li>
+        <li>Cornélio Procópio/ Cidade</li>
+      </ul>
+    </div>
+    <br />
+    <nav>
+      <img src="./assets/insta_ico.png" alt="" />
+      <img src="./assets/whatsapp_ico.png" alt="" />
+      <img src="./assets/avi_ico.png" alt="" />
+    </nav>
+    <p>&copy; 2025 - Todos os direitos reservados</p>
+  </footer>
+
+</body>
+
+</html>
